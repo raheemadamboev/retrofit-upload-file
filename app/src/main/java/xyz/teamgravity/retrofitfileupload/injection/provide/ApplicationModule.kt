e79606722541a657e7fc67294e79468a49ef1e49
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-15 12:45:13 +0500

    add ApplicationModule.kt

---
+++ app/src/main/java/xyz/teamgravity/retrofitfileupload/injection/provide/ApplicationModule.kt
@@ -0,0 +1,27 @@
+package xyz.teamgravity.retrofitfileupload.injection.provide
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import xyz.teamgravity.retrofitfileupload.data.remote.api.FileApi
+import xyz.teamgravity.retrofitfileupload.data.remote.constant.FileApiConst
+import xyz.teamgravity.retrofitfileupload.data.repository.FileRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideFileApi(): FileApi = Retrofit.Builder()
+        .baseUrl(FileApiConst.BASE_URL)
+        .build()
+        .create(FileApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideFileRepository(fileApi: FileApi): FileRepository = FileRepository(fileApi)
+}
