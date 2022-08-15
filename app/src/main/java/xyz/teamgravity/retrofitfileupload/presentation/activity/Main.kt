42e3286004a5e4047a3df3a40e2386aa43535450
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-15 14:09:37 +0500

    add entry point to main

--- app/src/main/java/xyz/teamgravity/retrofitfileupload/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/retrofitfileupload/presentation/activity/Main.kt
@@ -7,9 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.retrofitfileupload.presentation.screen.ImageUploadScreen
 import xyz.teamgravity.retrofitfileupload.presentation.theme.RetrofitFileUploadTheme
 
+@AndroidEntryPoint
 class Main : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,6 +22,7 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    ImageUploadScreen()
                 }
             }
         }
