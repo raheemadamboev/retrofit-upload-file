a5c248cb6f5fe3e49b0be2d211169356497836b6
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-15 12:44:18 +0500

    add constants for file api

---
+++ app/src/main/java/xyz/teamgravity/retrofitfileupload/data/remote/constant/FileApiConst.kt
@@ -0,0 +1,14 @@
+package xyz.teamgravity.retrofitfileupload.data.remote.constant
+
+object FileApiConst {
+
+    /**
+     * Base url
+     */
+    const val BASE_URL = "http://192.168.0.166/"
+
+    /**
+     * Image file type
+     */
+    const val TYPE_IMAGE = "image"
+}
