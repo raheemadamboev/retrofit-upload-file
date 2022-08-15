75830913ed34d18ab1e97998736c30560f4c0b91
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-15 14:10:54 +0500

    add post

--- app/src/main/java/xyz/teamgravity/retrofitfileupload/data/remote/api/FileApi.kt
+++ app/src/main/java/xyz/teamgravity/retrofitfileupload/data/remote/api/FileApi.kt
@@ -2,6 +2,7 @@ package xyz.teamgravity.retrofitfileupload.data.remote.api
 
 import okhttp3.MultipartBody
 import retrofit2.http.Multipart
+import retrofit2.http.POST
 import retrofit2.http.Part
 
 interface FileApi {
@@ -11,6 +12,7 @@ interface FileApi {
     ///////////////////////////////////////////////////////////////////////////
 
     @Multipart
+    @POST("/file")
     suspend fun uploadImage(
         @Part image: MultipartBody.Part,
     )
